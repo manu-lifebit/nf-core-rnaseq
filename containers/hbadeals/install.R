d5ff2e58e5f2a90e9865d895e75d06e265e54b62
cgpu <chatzipantsiou@gmail.com>
2020-05-07 00:44:44 +0100

    Adds @commit hash for HBA-DEALS repo for reproducibility

--- containers/hbadeals/install.R
+++ containers/hbadeals/install.R
@@ -1,4 +1,4 @@
 Sys.setenv(TAR = "/bin/tar")
-devtools::install_github("TheJacksonLaboratory/HBA-DEALS", upgrade="never")
+devtools::install_github("TheJacksonLaboratory/HBA-DEALS@5bbd9ca", upgrade="never")
 devtools::install_github("ropensci/piggyback@87f71e8", upgrade="never")
 
